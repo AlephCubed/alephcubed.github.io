877ee912ca2428ee96a5379c8d633e31136f5dfa
AlephCubed <jothebro500@gmail.com>
2025-04-28 11:35:19 -0700

    Copied over some code from `FirstWebsite` repo.
    Mostly doesn't work yet.

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,44 @@ enum Route {
 
 fn switch(route: Route) -> Html {
     match route {
-        Route::Root => html! { <h1>{ "Hello World!" }</h1> },
+        Route::Root => html! { 
+            <Main />
+        },
         Route::Test => html! { <h1>{ "Test" }</h1> },
+    }
+}
+
+#[function_component(Main)]
+fn root_page() -> Html {
+    html! { 
+        <div class="main">
+            <TitleSection />
+            <TestingSection text="More text for testing."/>
+            <TestingSection text="Even more text."/>
+        </div>
+    }
+}
+
+#[function_component(TitleSection)]
+fn title() -> Html {
+    html! {
+        <section>
+            <h1 class="title">{ "AlephCubed" }</h1>
+            <img src="static/images/DefaultPlanet.png" class="planet hidden"/>
+        </section>
+    }
+}
+
+#[derive(Properties, Eq, PartialEq)]
+struct TextProperty { 
+    pub text: String 
+}
+
+#[function_component(TestingSection)]
+fn test_section(text: &TextProperty) -> Html {
+    html! {
+        <section class="hidden">
+            <h1 class="title">{ &text.text }</h1>
+        </section>
     }
 }
